3c49733af5093d9205d39d52f1c0b491df0f448a
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-10-01 16:55:06 +0900

    Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Put your code
+
+// ROに格納されている値をR1に格納されている値の回数分足す方針で実装する
+// R2に初期値0を格納して、R2=R2+R0を繰り返せばいい？
+// C命令にラベルは使えない。A命令でラベルを使って、C命令でA,M,Dを使って計算する。
+    @i
+    M=1
+// sumではなくR2
+    @R2
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGT
+// 加算していく値はR0の値
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
